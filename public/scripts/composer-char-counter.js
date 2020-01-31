aa9e93854be512c6aa3cd2d8989ae76a00c96eb7
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-31 03:00:29 +0000

    added screenshots, cleaned up code, made helpers.js for helper functions

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,18 @@
 
-
 $(document).ready(function() {
-
   let max = 140;
-  let tweetable = true;
+
   $("#tweet-area").keyup(function() {
     let length = $(this).val().length;
     let remaining = max - length;
     $("#counter").text(remaining);
+
     if (remaining < 0) {
-      tweetable = false;
       document.getElementById('counter').style.color = "#ED6A5A";
       document.getElementById('tweet-area').style.color = "#ED6A5A";
     } else {
-      tweetable = true;
       document.getElementById('counter').style.color = "#545149";
       document.getElementById('tweet-area').style.color = "black";
     }
   });
-
-
 });
